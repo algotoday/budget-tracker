0f87590fe5a7f0257ce07768889732d02f308667
algotoday <100335717+algotoday@users.noreply.github.com>
2022-08-28 23:28:49 -0700

    eventlistener install serviceworker

--- public/service-worker.js
+++ public/service-worker.js
@@ -32,4 +32,30 @@ const FILES_TO_CACHE = [
         )
       })
 
-      self.addEventListener('fetch', function (e) {
+      self.addEventListener("install", function (e) {
+        e.waitUntil(
+         caches.open(CACHE_NAME).then((cache)=> {
+            console.log("Your files were pre-cached sucessfully!" + CACHE_NAME);
+            return cache.addAll(FILES_TO_CACHE);
+         })   
+        )
+      })
+
+      self.addEventListener("activate", function (e){
+        e.waitUntil(
+        caches.keys().then(function(keyList){
+            const cacheKeepList = keyList.filter(function(key){
+                return key.indexOf(APP_PREFIX);
+            })
+            cacheKeepList.push(CACHE_NAME);
+
+            return Promise.all(
+                keyList.map(function(key, i){
+                    if (cacheKeepList.indexOf(key)=== -1){
+                        return caches.delete(keyList[i]);
+                    }
+            })
+            );
+        })
+        );
+      });
